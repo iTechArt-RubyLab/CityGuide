da3bd0eb22f954ea66e547624d2b5f0300abeead
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-12-31 02:43:58 +0300

    CG-005/add bootstrap (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   devise_for :users, controllers: {
         sessions: 'users/sessions',
         registrations: 'users/registrations',
+        confirmations: 'users/confirmations',
+        passwords: 'users/passwords'
   }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_12_30_145942) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "is_admin"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
