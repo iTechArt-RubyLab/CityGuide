4e9413f0e1805b82f347e30ed33e550789a7685b
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-12-31 21:50:29 +0300

    CG-006/add omiauth (#5)

--- db/migrate/20211230145942_devise_create_users.rb
+++ db/migrate/20211230145942_devise_create_users.rb
@@ -15,6 +15,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Rememberable
       t.datetime :remember_created_at
       t.references :profilable, polymorphic: true
+
+      t.string :uid 
+      t.string :provider
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
