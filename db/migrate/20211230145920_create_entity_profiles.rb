d30ed3098c1adadb650919a20230805a8978dffd
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-01-03 23:50:40 +0300

    CG-009/add view from user profile (#10)
    * CG-009/add jquery configs
    
    * CG-009/add view to the form user

--- db/migrate/20211230145920_create_entity_profiles.rb
+++ db/migrate/20211230145920_create_entity_profiles.rb
@@ -1,13 +1,19 @@
 class CreateEntityProfiles < ActiveRecord::Migration[6.1]
   def change
     create_table :entity_profiles do |t|
+      t.string :name
+      t.string :surname
+      t.date :age
+      t.text :note
+      t.text :contact_info
       t.string :title_company
       t.float :min_price
-      t.time :min_time
+      t.integer :min_time
       t.float :price_one_hour
       t.float :price_two_hours
-      t.integer :time_work
-
+      t.time :start_work
+      t.time :end_work
+    
       t.timestamps
     end
   end
