0e5901c4519f17a30c705c8508c40138be3c112e
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-01-04 18:58:19 +0300

    CG-011/separate contactinfo and profile (#11)

--- app/models/human_profile.rb
+++ app/models/human_profile.rb
@@ -2,14 +2,18 @@
 #
 # Table name: human_profiles
 #
-#  id           :bigint           not null, primary key
-#  age          :date
-#  contact_info :text
-#  name         :string
-#  note         :text
-#  surname      :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id                     :bigint           not null, primary key
+#  age                    :date
+#  name                   :string
+#  note                   :text
+#  surname                :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  contact_information_id :bigint
+#
+# Indexes
+#
+#  index_human_profiles_on_contact_information_id  (contact_information_id)
 #
 class HumanProfile < ApplicationRecord
   has_one :user, as: :profilable
