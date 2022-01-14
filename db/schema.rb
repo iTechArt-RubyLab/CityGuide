0f67e98e07b394ef1e76dcdeaed63a8b255ffb3d
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-01-14 21:49:28 +0300

    CG-014/add profile and crud (#15)
    * add profile
    
    * CG-014/add connections

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_145942) do
+ActiveRecord::Schema.define(version: 2022_01_13_204637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.date "birthday"
+    t.text "note"
+    t.string "phone_number"
+    t.string "email_address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "is_admin"
