ec6dda5167ed73ceb7413380fe1d615e866cb16e
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-01-28 00:14:57 +0300

    CG-031/show only approved organizations in list (#32)

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,9 +4,9 @@ class OrganizationsController < ApplicationController
   # GET /organizations or /organizations.json
   def index
     @organizations = if params[:search_key]
-                       Organization.where('title LIKE ?', "%#{params[:search_key]}%")
+                       Organization.where('title LIKE ?', 'status = ?', "%#{params[:search_key]}%", 2)
                      else
-                       Organization.all
+                       Organization.where('status = ?', 2)
                      end
   end
 
