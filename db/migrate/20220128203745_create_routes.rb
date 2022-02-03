ee887caa5b5b2c98d2d6ef0b78a24f47fdefb9c8
Karina <karinaklevzhits@gmail.com>
2022-02-03 22:31:50 +0300

    CG-034/change name of column in route

--- db/migrate/20220128203745_create_routes.rb
+++ db/migrate/20220128203745_create_routes.rb
@@ -4,7 +4,7 @@ class CreateRoutes < ActiveRecord::Migration[6.1]
       t.references :tour_agency, null: false, foreign_key: {to_table: :organizations}
       t.references :visitor, null: false, foreign_key: {to_table: :users}
       t.date :start_date
-      t.date :start_end
+      t.date :end_date
       t.references :hotel, null: false, foreign_key: {to_table: :organizations}
       t.decimal :cost
       t.timestamps
