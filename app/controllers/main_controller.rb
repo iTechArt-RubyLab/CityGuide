d06caf8086244dff9e826e83d8e1ace2bc434893
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-02-06 01:49:38 +0300

    CG-037/map modification (#40)
    * CG-037/add panel to map route
    
    * CG-037/move map to main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,9 @@
 class MainController < ApplicationController
+  def index
+    @organizations = if params[:search_key]
+                       Organization.where('title LIKE ?', 'status = ?', "%#{params[:search_key]}%", 2)
+                     else
+                       Organization.where('status = ?', 2)
+                     end
+  end
 end
