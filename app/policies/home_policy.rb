fe57f65aa5ec44b39f9eae925c854d8281bfd4ee
SleepMode <72280071+KarinaKlevzhits@users.noreply.github.com>
2022-02-07 08:04:58 +0300

    CG-040/improve views (#43)
    * CG-040/improve main page
    
    * CG-040/add separate logic for views

---
+++ app/policies/home_policy.rb
@@ -0,0 +1,27 @@
+class HomePolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    true
+  end
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
