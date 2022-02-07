cadc0e461a2add416483e75dab5787965269dec2
Karina <karinaklevzhits@gmail.com>
2022-02-08 00:53:09 +0300

    fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
   skip_before_action :verify_authenticity_token
+
   def after_sign_in_path_for(_resource)
     if current_user.profile.nil?
       new_profile_path
     elsif current_user.is_admin
-      admin_view_url
+      admin_view_path
     else
-      main_url
+      main_path
     end
   end
 end
