b66ede1fc79b85fb9d19fea1940efaf050ed8700
Karina <karinaklevzhits@gmail.com>
2022-02-08 15:06:46 +0300

    rubocop fixes

--- spec/views/routes/index.html.erb_spec.rb
+++ spec/views/routes/index.html.erb_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "routes/index", type: :view do
+RSpec.describe 'routes/index', type: :view do
   before(:each) do
     assign(:routes, [
-      Route.create!(
-        tour_agency: nil,
-        user: nil,
-        hotel: nil,
-        cost: "9.99",
-        place: nil
-      ),
-      Route.create!(
-        tour_agency: nil,
-        user: nil,
-        hotel: nil,
-        cost: "9.99",
-        place: nil
-      )
-    ])
+             Route.create!(
+               tour_agency: nil,
+               user: nil,
+               hotel: nil,
+               cost: '9.99',
+               place: nil
+             ),
+             Route.create!(
+               tour_agency: nil,
+               user: nil,
+               hotel: nil,
+               cost: '9.99',
+               place: nil
+             )
+           ])
   end
 
-  it "renders a list of routes" do
+  it 'renders a list of routes' do
     render
-    assert_select "tr>td", text: nil.to_s, count: 2
-    assert_select "tr>td", text: nil.to_s, count: 2
-    assert_select "tr>td", text: nil.to_s, count: 2
-    assert_select "tr>td", text: "9.99".to_s, count: 2
-    assert_select "tr>td", text: nil.to_s, count: 2
+    assert_select 'tr>td', text: nil.to_s, count: 2
+    assert_select 'tr>td', text: nil.to_s, count: 2
+    assert_select 'tr>td', text: nil.to_s, count: 2
+    assert_select 'tr>td', text: '9.99'.to_s, count: 2
+    assert_select 'tr>td', text: nil.to_s, count: 2
   end
 end
--- spec/views/routes/show.html.erb_spec.rb
+++ spec/views/routes/show.html.erb_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "routes/show", type: :view do
+RSpec.describe 'routes/show', type: :view do
   before(:each) do
     @route = assign(:route, Route.create!(
-      tour_agency: nil,
-      user: nil,
-      hotel: nil,
-      cost: "9.99",
-      place: nil
-    ))
+                              tour_agency: nil,
+                              user: nil,
+                              hotel: nil,
+                              cost: '9.99',
+                              place: nil
+                            ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(//)
     expect(rendered).to match(//)
