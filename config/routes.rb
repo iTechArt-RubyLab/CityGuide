a035bf3b612d742e8f646ca9fe60de3840ecc89c
Karina <karinaklevzhits@gmail.com>
2022-02-08 13:50:46 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  root to: 'home#index'
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :routes do
     collection do
-    put 'approve',to: 'routes#approve'
-    get 'approve',to: 'routes#approve'
+      put 'approve', to: 'routes#approve'
+      get 'approve', to: 'routes#approve'
     end
   end
+
   resources :organizations do
     collection do
-    put 'approve',to: 'organizations#approve'
-    get 'approve',to: 'organizations#approve'
+      put 'approve', to: 'organizations#approve'
+      get 'approve', to: 'organizations#approve'
     end
   end
+
   resources :places
   resources :profiles
-  root to: 'home#index'
-  get 'main',to: 'main#index'
-  get 'admin_view',to: 'admin_view#index'
-  mount Sidekiq::Web => '/sidekiq'
+  get 'main', to: 'main#index'
+  get 'admin_view', to: 'admin_view#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
