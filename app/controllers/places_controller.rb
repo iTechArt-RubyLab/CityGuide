61456f92cddb95d93f7857dcd69f504522c4328a
Karina <karinaklevzhits@gmail.com>
2022-02-08 14:04:38 +0300

    delete render json and fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,49 +20,37 @@ class PlacesController < ApplicationController
   # POST /places or /places.json
   def create
     @place = Place.new(place_params)
-    respond_to do |format|
-      if @place.save
-        format.html { redirect_to place_url(@place), notice: 'Place was successfully created.' }
-        format.json { render :show, status: :created, location: @place }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @place.errors, status: :unprocessable_entity }
-      end
+    if @place.save
+      redirect_to place_url(@place), notice: 'Place was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+end
 
-  # PATCH/PUT /places/1 or /places/1.json
-  def update
-    respond_to do |format|
-      if @place.update(place_params)
-        format.html { redirect_to place_url(@place), notice: 'Place was successfully updated.' }
-        format.json { render :show, status: :ok, location: @place }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @place.errors, status: :unprocessable_entity }
-      end
-    end
+# PATCH/PUT /places/1 or /places/1.json
+def update
+  if @place.update(place_params)
+    redirect_to place_url(@place), notice: 'Place was successfully updated.'
+  else
+    render :edit, status: :unprocessable_entity
   end
+end
 
-  # DELETE /places/1 or /places/1.json
-  def destroy
-    @place.destroy
-
-    respond_to do |format|
-      format.html { redirect_to places_url, notice: 'Place was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+# DELETE /places/1 or /places/1.json
+def destroy
+  @place.destroy
+  redirect_to places_url, notice: 'Place was successfully destroyed.'
+end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def place
-    @place = Place.find(params[:id])
-  end
+# Use callbacks to share common setup or constraints between actions.
+def place
+  @place = Place.find(params[:id])
+end
 
-  # Only allow a list of trusted parameters through.
-  def place_params
-    params.require(:place).permit(:name, :latitude, :longitude)
-  end
+# Only allow a list of trusted parameters through.
+def place_params
+  params.require(:place).permit(:name, :latitude, :longitude)
 end
