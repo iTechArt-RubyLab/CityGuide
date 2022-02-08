488f7f0bcb3c28534526c5edb62d278ae28b7bd1
Karina <karinaklevzhits@gmail.com>
2022-02-08 12:33:53 +0300

    fix create mailer

--- app/mailers/create_notification_mailer.rb
+++ app/mailers/create_notification_mailer.rb
@@ -1,13 +1,13 @@
 class CreateNotificationMailer < ApplicationMailer
-    def create_notification_route(user,notification_message)
-        @user = params[:user]
-        @notification_message = notification_message
-        mail(to: @user.email, subject: "#{@notification_message}")
-    end
+  def create_notification_route(_user, notification_message)
+    @user = params[:user]
+    @notification_message = notification_message
+    mail(to: @user.email, subject: @notification_message.to_s)
+  end
 
-    def create_notification_organization(user,notification_message)
-        @user = params[:user]
-        @notification_message = notification_message
-        mail(to: @user.email, subject: "#{@notification_message}")
-    end
+  def create_notification_organization(_user, notification_message)
+    @user = params[:user]
+    @notification_message = notification_message
+    mail(to: @user.email, subject: @notification_message.to_s)
+  end
 end
