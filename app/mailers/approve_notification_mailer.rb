f6951b477ca3f8d41c48ba139a344a19b7df4c02
Karina <karinaklevzhits@gmail.com>
2022-02-08 12:18:40 +0300

    fix approve mailer

--- app/mailers/approve_notification_mailer.rb
+++ app/mailers/approve_notification_mailer.rb
@@ -1,10 +1,11 @@
 class ApproveNotificationMailer < ApplicationMailer
-    def approve_notification_route(user)
-        @user = params[:user]
-        mail(to: @user.email, subject: "Your route has been approved")
-    end
-    def approve_notification_organization(user)
-        @user = params[:user]
-        mail(to: @user.email, subject: "Your organization has been approved")
-    end
+  def approve_notification_route(_user)
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Your route has been approved')
+  end
+
+  def approve_notification_organization(_user)
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Your organization has been approved')
+  end
 end
