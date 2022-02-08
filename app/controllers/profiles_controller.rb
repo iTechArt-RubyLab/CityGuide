f8c6f7fb3bdba89ff5719038e495b8c5ea94910d
Karina <karinaklevzhits@gmail.com>
2022-02-08 14:07:56 +0300

    delete json and fix

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,49 +21,38 @@ class ProfilesController < ApplicationController
   def create
     @profile = current_user.build_profile(profile_params)
 
-    respond_to do |format|
-      if @profile.save
-        format.html { redirect_to profile_url(@profile), notice: 'Profile was successfully created.' }
-        format.json { render :show, status: :created, location: @profile }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
+    if @profile.save
+      redirect_to profile_url(@profile), notice: 'Profile was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+end
 
-  # PATCH/PUT /profiles/1 or /profiles/1.json
-  def update
-    respond_to do |format|
-      if @profile.update(profile_params)
-        format.html { redirect_to profile_url(@profile), notice: 'Profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @profile }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
-    end
+# PATCH/PUT /profiles/1 or /profiles/1.json
+def update
+  if @profile.update(profile_params)
+    redirect_to profile_url(@profile), notice: 'Profile was successfully updated.'
+  else
+    render :edit, status: :unprocessable_entity
   end
+end
 
-  # DELETE /profiles/1 or /profiles/1.json
-  def destroy
-    @profile.destroy
+# DELETE /profiles/1 or /profiles/1.json
+def destroy
+  @profile.destroy
 
-    respond_to do |format|
-      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  redirect_to profiles_url, notice: 'Profile was successfully destroyed.'
+end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_profile
-    @profile = Profile.find(params[:id])
-  end
+# Use callbacks to share common setup or constraints between actions.
+def set_profile
+  @profile = Profile.find(params[:id])
+end
 
-  # Only allow a list of trusted parameters through.
-  def profile_params
-    params.require(:profile).permit(:name, :surname, :birthday, :note, :phone_number, :email_address, :avatar)
-  end
+# Only allow a list of trusted parameters through.
+def profile_params
+  params.require(:profile).permit(:name, :surname, :birthday, :note, :phone_number, :email_address, :avatar)
 end
