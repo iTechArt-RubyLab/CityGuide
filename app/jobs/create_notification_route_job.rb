df97c29048f3d3c9e284a1c5f3b520a5a3bbd760
Karina <karinaklevzhits@gmail.com>
2022-02-08 12:17:18 +0300

    fix route job

--- app/jobs/create_notification_route_job.rb
+++ app/jobs/create_notification_route_job.rb
@@ -1,7 +1,7 @@
 class CreateNotificationRouteJob < ApplicationJob
-    queue_as :default
-  
-    def perform(user,notification_messsage)
-      CreateNotificationMailer.with(user: user).create_notification_route(user,notification_messsage).deliver_now
-    end
+  queue_as :default
+
+  def perform(user, notification_messsage)
+    CreateNotificationMailer.with(user: user).create_notification_route(user, notification_messsage).deliver_now
   end
+end
