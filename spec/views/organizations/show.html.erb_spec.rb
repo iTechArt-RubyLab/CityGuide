4975929c68eda99fcfa92eb70aa2f58a521ed387
Karina <karinaklevzhits@gmail.com>
2022-02-08 15:06:46 +0300

    rubocop fixes

--- spec/views/organizations/show.html.erb_spec.rb
+++ spec/views/organizations/show.html.erb_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "organizations/show", type: :view do
+RSpec.describe 'organizations/show', type: :view do
   before(:each) do
     @organization = assign(:organization, Organization.create!(
-      title: "Title",
-      min_price: 2.5,
-      min_time: 3,
-      price_one_hour: 4.5,
-      price_two_hours: 5.5,
-      type_of_services: 6,
-      status: 7
-    ))
+                                            title: 'Title',
+                                            min_price: 2.5,
+                                            min_time: 3,
+                                            price_one_hour: 4.5,
+                                            price_two_hours: 5.5,
+                                            type_of_services: 6,
+                                            status: 7
+                                          ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/2.5/)
--- spec/views/profiles/show.html.erb_spec.rb
+++ spec/views/profiles/show.html.erb_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "profiles/show", type: :view do
+RSpec.describe 'profiles/show', type: :view do
   before(:each) do
     @profile = assign(:profile, Profile.create!(
-      name: "Name",
-      surname: "Surname",
-      note: "MyText",
-      phone_number: "Phone Number",
-      email_address: "Email Address"
-    ))
+                                  name: 'Name',
+                                  surname: 'Surname',
+                                  note: 'MyText',
+                                  phone_number: 'Phone Number',
+                                  email_address: 'Email Address'
+                                ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/Surname/)
